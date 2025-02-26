fded9f11e32c0174bef98c7cbe2c2e41490fed4f
GudipatiPallavi <gudipatipallavirao@gmail.com>
2025-02-26 17:17:31 -0500

    changes

--- website/app.js
+++ website/app.js
@@ -1,60 +1,60 @@
-function scrollAppear() {
-  var appearItem = document.querySelector('.first');
-  var itemPosition = appearItem.getBoundingClientRect().top;
-  var screenPosition = window.innerHeight / 1.5;
+// function scrollAppear() {
+//   var appearItem = document.querySelector('.first');
+//   var itemPosition = appearItem.getBoundingClientRect().top;
+//   var screenPosition = window.innerHeight / 1.5;
 
-  var appearItem2 = document.querySelector('.second');
-  var itemPosition2 = appearItem2.getBoundingClientRect().top;
-  var screenPosition2 = window.innerHeight / 1.5;
+//   var appearItem2 = document.querySelector('.second');
+//   var itemPosition2 = appearItem2.getBoundingClientRect().top;
+//   var screenPosition2 = window.innerHeight / 1.5;
 
-  var appearItem3 = document.querySelector('.third');
-  var itemPosition3 = appearItem3.getBoundingClientRect().top;
-  var screenPosition3 = window.innerHeight / 1.5;
+//   var appearItem3 = document.querySelector('.third');
+//   var itemPosition3 = appearItem3.getBoundingClientRect().top;
+//   var screenPosition3 = window.innerHeight / 1.5;
   
-  var itemPosition4 = document.querySelector('footer').getBoundingClientRect().top;
-  var screenPosition4 = window.innerHeight;
+//   var itemPosition4 = document.querySelector('footer').getBoundingClientRect().top;
+//   var screenPosition4 = window.innerHeight;
 
-  if (itemPosition < screenPosition) {
-    appearItem.classList.remove('appear');
-    appearItem.classList.add('appear');
-  } else if (itemPosition > screenPosition) {
-    appearItem.classList.remove('appear');
-    appearItem.classList.add('appear');
-  }
+//   if (itemPosition < screenPosition) {
+//     appearItem.classList.remove('appear');
+//     appearItem.classList.add('appear');
+//   } else if (itemPosition > screenPosition) {
+//     appearItem.classList.remove('appear');
+//     appearItem.classList.add('appear');
+//   }
 
-  if (itemPosition2 < screenPosition2) {
-    appearItem2.classList.remove('appear');
-    appearItem2.classList.add('appear');
-  } else if (itemPosition2 > screenPosition2) {
-    appearItem2.classList.remove('appear');
-    appearItem2.classList.add('appear');
-  }
+//   if (itemPosition2 < screenPosition2) {
+//     appearItem2.classList.remove('appear');
+//     appearItem2.classList.add('appear');
+//   } else if (itemPosition2 > screenPosition2) {
+//     appearItem2.classList.remove('appear');
+//     appearItem2.classList.add('appear');
+//   }
 
-  if (itemPosition3 < screenPosition3) {
-    appearItem3.classList.remove('appear');
-    appearItem3.classList.add('appear');
-  } else if (itemPosition3 > screenPosition3) {
-    appearItem3.classList.remove('appear');
-    appearItem3.classList.add('appear');
-  }
+//   if (itemPosition3 < screenPosition3) {
+//     appearItem3.classList.remove('appear');
+//     appearItem3.classList.add('appear');
+//   } else if (itemPosition3 > screenPosition3) {
+//     appearItem3.classList.remove('appear');
+//     appearItem3.classList.add('appear');
+//   }
 
-  if (itemPosition4 < screenPosition4) {
-    document.querySelector('#intern-cta').classList.add('appear');
-    document.querySelector('#intern-cta').classList.remove('appear');
-  } else if (itemPosition4 > screenPosition4) {
-    document.querySelector('#intern-cta').classList.add('appear');
-    document.querySelector('#intern-cta').classList.remove('appear');
-  }
-}
+//   if (itemPosition4 < screenPosition4) {
+//     document.querySelector('#intern-cta').classList.add('appear');
+//     document.querySelector('#intern-cta').classList.remove('appear');
+//   } else if (itemPosition4 > screenPosition4) {
+//     document.querySelector('#intern-cta').classList.add('appear');
+//     document.querySelector('#intern-cta').classList.remove('appear');
+//   }
+// }
 
-function toggle() {
-  var menu = document.getElementById('navs');
+// function toggle() {
+//   var menu = document.getElementById('navs');
 
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-  }
+//   if (menu.style.display === "block") {
+//     menu.style.display = "none";
+//   } else {
+//     menu.style.display = "block";
+//   }
 
   document.addEventListener("DOMContentLoaded", function () {
     var typed = new typed("#typing-text", {
@@ -69,9 +69,9 @@ function toggle() {
   
 
 
-};
+// };
 
-var ham = document.querySelector('.ham');
+// var ham = document.querySelector('.ham');
 
-ham.addEventListener('click', toggle);
-window.addEventListener('scroll', scrollAppear);
+// ham.addEventListener('click', toggle);
+// window.addEventListener('scroll', scrollAppear);
