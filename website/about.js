960e41c9a9b1d857c40a74de4b344656e47170dd
GudipatiPallavi <gudipatipallavirao@gmail.com>
2025-03-10 17:48:32 -0400

    changes

--- website/about.js
+++ website/about.js
@@ -16,11 +16,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     if (document.getElementById("typing-text")) {  // Check if element exists
         let typed = new Typed("#typing-text", {
-            strings: ["Product Manager", "Product Designer"],
+            strings: ["Product Manager", "Product Designer", "Software Engineer"],
             typeSpeed: 80, // Slower typing speed (increase if needed)
             backSpeed: 40, // Slower deleting speed
-            startDelay: 500,  // Delay before typing starts
-            backDelay: 2000, // **Pauses for 2 seconds before deleting**
+            startDelay: 500, // Delay before typing starts
+            backDelay: 2000, // Pauses for 2 seconds before deleting
             loop: true
         });
     } else {
