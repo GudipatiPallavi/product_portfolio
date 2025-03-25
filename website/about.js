d9183952b5083ab2171f78375f1fdc97d0b0347f
GudipatiPallavi <gudipatipallavirao@gmail.com>
2025-03-24 21:08:12 -0400

    learnings - major changes

--- website/about.js
+++ website/about.js
@@ -12,3 +12,33 @@
 // }
 
 // window.addEventListener('scroll', scrollAppear);
+
+const typewriterText = document.getElementById("typewriter");
+const roles = ["Product Manager", "Product Designer", "Software Engineer", "Data Enthusiast"];
+let roleIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function type() {
+  const current = roles[roleIndex];
+  const partial = current.substring(0, charIndex);
+  typewriterText.textContent = partial;
+
+  if (!isDeleting && charIndex < current.length) {
+    charIndex++;
+    setTimeout(type, 120);
+  } else if (isDeleting && charIndex > 0) {
+    charIndex--;
+    setTimeout(type, 60);
+  } else {
+    isDeleting = !isDeleting;
+    if (!isDeleting) {
+      roleIndex = (roleIndex + 1) % roles.length;
+    }
+    setTimeout(type, 1000);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(type, 500);
+});
